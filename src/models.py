e848bdf616b8453438c6217d88f862c1bc0fb6fd
joshs <jsteiner@utexas.edu>
2025-08-02 18:09:29 -0400

    Simple CNN 8/2/25

--- src/models.py
+++ src/models.py
@@ -9,11 +9,44 @@ INPUT_MEAN = [0.2788, 0.2657, 0.2629]
 INPUT_STD = [0.2064, 0.1944, 0.2252]
 
 
-### insert models here
+class SimpleCNN(nn.Module):
+    def __init__(self, num_classes=4):
+        super().__init__()
+
+        # CNN block
+        self.conv_block = nn.Sequential(                    # input: 3x128x128
+            nn.Conv2d(3, 16, kernel_size=3, padding=1),     # 16x128x128
+            nn.ReLU(),
+            nn.MaxPool2d(2),                                # 16x64x64
+
+            nn.Conv2d(16, 32, kernel_size=3, padding=1),    # 32x64x64
+            nn.ReLU(),
+            nn.MaxPool2d(2),                                # 32x32x32
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),    # 64x32x32
+            nn.ReLU(),
+            nn.MaxPool2d(2)                                 # 64x16x16
+        )
+
+        # classifier
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(64 * 16 * 16, 128),
+            nn.ReLU(),
+            nn.Linear(128, num_classes)
+        )
+
+    # forward pass
+    def forward(self, x):
+        x = self.conv_block(x)
+        x = self.classifier(x)
+        return x
 
 
 
-MODEL_FACTORY = {}
+MODEL_FACTORY = {
+    "simpleCNN": SimpleCNN
+}
 
 
 def load_model(
@@ -37,6 +70,8 @@ def load_model(
                 f"Failed to load {model_path.name}, make sure the default model arguments are set correctly"
 
             ) from e
+        
+    return m
         
 
 def save_model(model: torch.nn.Module) -> str:
